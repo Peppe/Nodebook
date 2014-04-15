c910ad59056919065d5327c27b7b5404c8d23f3f
Jens Jansson <jens.jansson@vaadin.com>
2014-04-15 14:44:27 +0300

    table listing with selection mechanism implemented

---
+++ public/javascripts/global.js
@@ -0,0 +1,28 @@
+
+// DOM ready
+$(document).ready(function() {
+    populateTable();
+    $('#users tbody').delegate('tr', 'click', select);
+});
+
+function populateTable(){
+  var tableContent = '';
+  console.log('adding rows to table')
+  $.getJSON('/users', function(data){
+    $.each(data, function(){
+      tableContent += '<tr>';
+      tableContent += '<td>' + this.first + '</td>';
+      tableContent += '<td>' + this.last + '</td>';
+      tableContent += '<td>' + this.email + '</td>';
+      tableContent += '<td>' + this.phone + '</td>';
+      tableContent += '</tr>';
+      $('#users tbody').html(tableContent);
+    });
+  });
+}
+
+function select(event){
+  console.log('changing selection');
+  $('.selected').removeClass('selected')
+  $(this).addClass('selected');
+}
